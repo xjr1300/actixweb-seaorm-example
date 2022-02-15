c02cfbfd337e15bbe2d19068076b10765a1b7e11
Yasunori Ishiguro <ishiguro-y@dainichi-consul.co.jp>
2022-02-15 11:18:12 +0900

    ログを出力するように実装

--- services/src/bin/web_api_server.rs
+++ services/src/bin/web_api_server.rs
@@ -1,9 +1,36 @@
 use actix_web::{App, HttpServer};
+use anyhow::anyhow;
 
 use adapters::handlers::hello;
 
+use common::ENV_VALUES;
+
+/// ログの出力方法を設定する。
+///
+/// # Returns
+///
+/// `Result`。返却される`Result`の内容は以下の通り。
+///
+/// * `Ok`: ()。
+/// * `Err`: エラー内容。
+fn init_logging() -> anyhow::Result<()> {
+    // ロギング設定ファイルを開く。
+    match log4rs::init_file(&ENV_VALUES.log4rs_config, Default::default()) {
+        Ok(_) => Ok(()),
+        Err(err) => Err(anyhow!(
+            "ファイル({})からロギング設定を得られません。{:?}",
+            ENV_VALUES.log4rs_config,
+            err,
+        )),
+    }
+}
+
+/// Web APIサーバーのエントリポイント
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // ロギングを設定
+    init_logging().unwrap();
+
     HttpServer::new(|| App::new().service(hello))
         .bind("127.0.0.1:8000")?
         .run()
